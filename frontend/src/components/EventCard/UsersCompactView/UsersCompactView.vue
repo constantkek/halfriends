ebe7cfc448ea42d8a23ce22bea39e2cb42c3a248
coshtya <kkletsyn@mail.ru>
2022-07-21 15:07:18 +0300

    Add user colors

--- frontend/src/components/EventCard/UsersCompactView/UsersCompactView.vue
+++ frontend/src/components/EventCard/UsersCompactView/UsersCompactView.vue
@@ -4,7 +4,8 @@
       v-for="user in slicedUsers"
       :key="user.firstname"
       class="avatar"
-      size="small"
+      :style="`background-color: ${getColorByUsernameHash(user)}`"
+      :size="34"
     >
       <div>
         <span>{{ `${user.firstname[0]}${user.lastname[0]}` }}</span>
@@ -27,6 +28,24 @@ export default class UsersCompactView extends Vue {
   public get slicedUsers(): User[] {
     return this.users.slice(0, this.limit);
   }
+
+  private fullname(user: User): string {
+    return `${user.firstname} ${user.lastname}`;
+  }
+
+  public getColorByUsernameHash(user: User): string {
+    let hash = 9237;
+    this.fullname(user).split('').forEach((char) => {
+      hash = ((hash << 5) + hash) + char.charCodeAt(0);
+    });
+    const r = ((hash & 0xFF0000) >> 16).toString(16);
+    const g = ((hash & 0x00FF00) >> 8).toString(16);
+    const b = (hash & 0x0000FF).toString(16);
+    return "#" +
+      (r.substring(r.length - 2)) +
+      (g.substring(g.length - 2)) +
+      (b.substring(b.length - 2)); 
+  }
 }
 </script>
 
@@ -38,14 +57,18 @@ export default class UsersCompactView extends Vue {
 .avatar {
   border: 2px solid white;
   margin-left: -7px;
-  font-size: 9px;
+  font-size: 11px;
+  font-weight: 600;
 }
 .avatar>div {
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 22px;
-  height: 24px;
+  width: 26px;
+  height: 30px;
+}
+.avatar:last-child>div {
+  width: 30px;
 }
 .avatar>div>span {
   display: flex;
